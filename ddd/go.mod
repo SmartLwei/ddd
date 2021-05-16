d224a932fada677cf4cd1144a34c7daece9ee418
wei.liu <wei.liu@matrixport.com>
2021-05-16 13:25:42 +0800

    import log module

--- ddd/go.mod
+++ ddd/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/swaggo/gin-swagger v1.3.0
 	github.com/swaggo/swag v1.5.1
-	golang.org/x/tools v0.0.0-20191029190741-b9c20aec41a5 // indirect
+	go.uber.org/zap v1.16.0
 	google.golang.org/grpc v1.36.0
 	gopkg.in/yaml.v2 v2.2.8
 	gorm.io/driver/mysql v1.0.4
---
+++ ddd/log/log.go
@@ -0,0 +1,105 @@
+package log
+
+import (
+	"go.uber.org/zap"
+	zapcore "go.uber.org/zap/zapcore"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+var sugar *zap.SugaredLogger
+var logger *zap.Logger
+
+func getLogLevel(level string) zapcore.Level {
+	level = strings.ToUpper(level)
+	switch level {
+	case "DEBUG":
+		return zapcore.DebugLevel
+	case "INFO":
+		return zapcore.InfoLevel
+	case "WARN":
+		return zapcore.WarnLevel
+	case "ERROR":
+		return zapcore.ErrorLevel
+	}
+	panic("unknown level: " + level)
+}
+
+// Init the log module
+func Init(logLevel string, ws ...io.Writer) {
+	level := getLogLevel(logLevel)
+	var encoderCfg = zapcore.EncoderConfig{
+		TimeKey:       "ts",
+		LevelKey:      "level",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		FunctionKey:   zapcore.OmitKey,
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace",
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   zapcore.LowercaseLevelEncoder,
+		EncodeTime: func(t time.Time, encoder zapcore.PrimitiveArrayEncoder) {
+			encoder.AppendString(t.Format("2006-01-02 15:04:05.000 MST"))
+		},
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+	encoder := zapcore.NewJSONEncoder(encoderCfg)
+
+	var cores []zapcore.Core
+	cores = append(cores, zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), level))
+	for _, w := range ws {
+		cores = append(cores, zapcore.NewCore(encoder, zapcore.AddSync(w), level))
+	}
+	tree := zapcore.NewTee(cores...)
+
+	if logger != nil {
+		_ = logger.Sync()
+	}
+	if sugar != nil {
+		_ = sugar.Sync()
+	}
+	logger = zap.New(tree)
+	sugar = logger.Sugar()
+}
+
+func Sync() {
+	if logger != nil {
+		_ = logger.Sync()
+		_ = sugar.Sync()
+	}
+}
+
+func DebugZ(msg string, fields ...zap.Field) {
+	logger.Debug(msg, fields...)
+}
+
+func InfoZ(msg string, fields ...zap.Field) {
+	logger.Info(msg, fields...)
+}
+
+func WarnZ(msg string, fields ...zap.Field) {
+	logger.Warn(msg, fields...)
+}
+
+func ErrorZ(msg string, fields ...zap.Field) {
+	logger.Error(msg, fields...)
+}
+
+func Debug(msg string, args ...interface{}) {
+	sugar.Debugw(msg, args...)
+}
+
+func Info(msg string, args ...interface{}) {
+	sugar.Debugw(msg, args...)
+}
+
+func Warn(msg string, args ...interface{}) {
+	sugar.Debugw(msg, args...)
+}
+
+func Error(msg string, args ...interface{}) {
+	sugar.Debugw(msg, args...)
+}
